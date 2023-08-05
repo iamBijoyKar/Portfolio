39dc72da3cf3a3ebcc67c958a7bbcf39c8dd6989
iamBijoyKar <bijoypoco12345@gmail.com>
2023-08-05 19:07:21 +0530

    skills page added

--- src/components/Home/SkillCard.jsx
+++ src/components/Home/SkillCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
+import { CardActionArea } from '@mui/material'
 
 SkillCard.propTypes = {
   children: PropTypes.node.isRequired,
@@ -11,17 +12,25 @@ export default function SkillCard({ children, title }) {
   const colorTheme = useSelector((state) => state.mode.colorTheme)
 
   return (
-    <div
-      className="p-4 w-[108px] h-[110px] rounded flex flex-col items-center justify-start gap-2 relative cursor-pointer shadow-lg"
-      style={{ backgroundColor: colorTheme.secondaryBg }}
-    >
-      {children}
-      <span
-        style={{ color: colorTheme.primaryText }}
-        className="absolute bottom-1 font-bold text-md"
+    <div className="">
+      <CardActionArea
+        sx={{
+          width: 'fin-content',
+        }}
       >
-        {title}
-      </span>
+        <div
+          className="p-4 w-[108px] h-[110px] rounded flex flex-col items-center justify-start gap-2 relative cursor-pointer shadow-lg"
+          style={{ backgroundColor: colorTheme.secondaryBg }}
+        >
+          {children}
+          <span
+            style={{ color: colorTheme.primaryText }}
+            className="absolute bottom-1 font-bold text-md"
+          >
+            {title}
+          </span>
+        </div>
+      </CardActionArea>
     </div>
   )
 }
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Layout from '../Layouts/Layout'
 import BackBtn from '../components/BackBtn'
+import SkillsIntro from '../components/Skills/Intro'
+import Skills from '../components/Skills/Skills'
 
 export default function SkillsPage() {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-start min-h-screen mt-[4.5rem]">
         <BackBtn />
-        <h1 className="text-4xl font-bold">Skills Page</h1>
+        <SkillsIntro />
+        <Skills />
       </div>
     </Layout>
   )
