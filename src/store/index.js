eb591a3d3ca8f504c4fa31b6d6eac6dd4b83abd5
iamBijoyKar <bijoypoco12345@gmail.com>
2023-08-06 16:44:08 +0530

    new states added

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,17 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 import { knowMode, setMode, getTheme, setTheme } from './utils'
+import {
+  getContactFormName,
+  getContactFormEmail,
+  getContactFormMessage,
+  setContactFormEmail,
+  setContactFormMessage,
+  setContactFormName,
+  setIsHireMeSubmitted,
+  setIsSayHiSubmitted,
+  getIsHireMeSubmitted,
+  getIsSayHiSubmitted,
+} from './utils'
 
 const initialModeState = {
   darkMode: knowMode(),
@@ -8,6 +20,14 @@ const initialModeState = {
 
 const initialConfettiState = {
   confetti: false,
+}
+
+const initialContactFormState = {
+  name: getContactFormName() ? getContactFormName() : '',
+  email: getContactFormEmail() ? getContactFormEmail() : '',
+  message: getContactFormMessage() ? getContactFormMessage() : '',
+  isHireMeSubmitted: getIsHireMeSubmitted() ? true : false,
+  isSayHiSubmitted: getIsSayHiSubmitted() ? true : false,
 }
 
 const modeSlice = createSlice({
@@ -36,12 +56,49 @@ const confettiSlice = createSlice({
   },
 })
 
+const contactFormSlice = createSlice({
+  name: 'contactForm',
+  initialState: initialContactFormState,
+  reducers: {
+    updateName(state, action) {
+      state.name = action.payload
+      setContactFormName(state.name)
+    },
+    updateEmail(state, action) {
+      state.email = action.payload
+      setContactFormEmail(state.email)
+    },
+    updateMessage(state, action) {
+      state.message = action.payload
+      setContactFormMessage(state.message)
+    },
+    updateIsHireMeSubmitted(state, action) {
+      if (state.isSayHiSubmitted && action.payload) {
+        state.isSayHiSubmitted = false
+        setIsSayHiSubmitted(false)
+      }
+      state.isHireMeSubmitted = action.payload
+      setIsHireMeSubmitted(state.isHireMeSubmitted)
+    },
+    updateIsSayHiSubmitted(state, action) {
+      if (state.isHireMeSubmitted && action.payload) {
+        state.isHireMeSubmitted = false
+        setIsHireMeSubmitted(false)
+      }
+      state.isSayHiSubmitted = action.payload
+      setIsSayHiSubmitted(state.isSayHiSubmitted)
+    },
+  },
+})
+
 export const store = configureStore({
   reducer: {
     mode: modeSlice.reducer,
     confetti: confettiSlice.reducer,
+    contactForm: contactFormSlice.reducer,
   },
 })
 
 export const modeActions = modeSlice.actions
 export const confettiActions = confettiSlice.actions
+export const contactFormActions = contactFormSlice.actions
