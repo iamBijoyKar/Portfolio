22669eb6dbdce6b2d151cdb8df08d75088ae3332
Bijoy Kar <bijoykar54321@gmail.com>
2024-01-13 18:46:15 +0530

    [UPDATE]: Contact form UI background changed 📱

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -21,6 +21,7 @@ import SayHi from './SayHi'
 
 export default function ContactForm() {
   const colorTheme = useSelector((state) => state.mode.colorTheme)
+  const isDarkMode = useSelector((state) => state.mode.darkMode)
   const name = useSelector((state) => state.contactForm.name)
   const email = useSelector((state) => state.contactForm.email)
   const message = useSelector((state) => state.contactForm.message)
@@ -114,14 +115,7 @@ export default function ContactForm() {
           className={`max-w-[400px] w-full mt-4 rounded-lg`}
         >
           {/* <h1 className="text-4xl font-bold text-center mb-4">Contact Me</h1> */}
-          <div
-            style={{
-              background: 'url(/images/form-bg2.jpg)',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-            }}
-            className="shadow-xl rounded-lg "
-          >
+          <div className="shadow-xl rounded-lg bg-slate-100  ">
             <Card
               variant="outlined"
               className="flex flex-col items-center "
@@ -146,7 +140,7 @@ export default function ContactForm() {
 
               <CardContent className="w-full">
                 <TextField
-                  id="fullWidth"
+                  name="name"
                   label="Name"
                   variant="outlined"
                   type="text"
@@ -159,7 +153,7 @@ export default function ContactForm() {
                   fullWidth
                 />
                 <TextField
-                  id="fullWidth"
+                  name="email"
                   label="Email"
                   inputMode="email"
                   type="email"
@@ -173,7 +167,7 @@ export default function ContactForm() {
                   }}
                 />
                 <TextField
-                  id="fullWidth"
+                  name="message"
                   label="Message"
                   variant="outlined"
                   sx={{ marginTop: 2 }}
